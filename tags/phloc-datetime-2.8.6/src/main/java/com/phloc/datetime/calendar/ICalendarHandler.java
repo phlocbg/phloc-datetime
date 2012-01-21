ca3b8dcee2b76bf7eadc24b068706c84e6f00773
ph@phloc.com <ph@phloc.com@47ddc20f-e4ca-fe23-0b93-bd64df41ddb8>
2012-01-21 20:03:53 +0000

    [maven-release-plugin]  copy for tag phloc-datetime-2.8.6

---
+++ tags/phloc-datetime-2.8.6/src/main/java/com/phloc/datetime/calendar/ICalendarHandler.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.datetime.calendar;
+
+import java.util.List;
+
+import org.joda.time.LocalDate;
+
+
+public interface ICalendarHandler
+{
+  void onDay (LocalDate aDay, List <ICalendarEntry> aEntries);
+
+  void onWeekStart (LocalDate aDate);
+
+  void onWeekEnd (LocalDate aDate);
+
+  void onMonthStart (LocalDate aDate);
+
+  void onMonthEnd (LocalDate aDate);
+
+  void onViewStart (ECalendarView aView);
+
+  void onViewEnd (ECalendarView aView);
+
+  CalendarSettings getCalendarSettings ();
+
+  void setSelectedDate (LocalDate aDate);
+
+  LocalDate getSelectedDate ();
+}
---
+++ tags/phloc-datetime-2.8.6/src/test/java/com/phloc/datetime/config/PDTConfigTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.datetime.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.joda.time.DateTimeZone;
+import org.junit.Test;
+
+/**
+ * Test class for class {@link PDTConfig}.
+ * 
+ * @author philip
+ */
+public final class PDTConfigTest
+{
+  @Test
+  public void testTimeZones ()
+  {
+    DateTimeZone aDTZ = PDTConfig.getDefaultDateTimeZone ();
+    assertNotNull (aDTZ);
+
+    aDTZ = PDTConfig.getDateTimeZoneUTC ();
+    assertNotNull (aDTZ);
+    assertEquals ("UTC", aDTZ.getID ());
+
+    assertNotNull (PDTConfig.getDefaultChronologyUTC ());
+    assertEquals (PDTConfig.getDateTimeZoneUTC (), PDTConfig.getDefaultChronologyUTC ().getZone ());
+
+    assertNotNull (PDTConfig.getDefaultChronologyWithoutDateTimeZone ());
+    assertEquals (DateTimeZone.getDefault (), PDTConfig.getDefaultChronologyWithoutDateTimeZone ().getZone ());
+
+    try
+    {
+      // Invalid
+      assertFalse (PDTConfig.setDefaultDateTimeZoneID ("does not exist").isSuccess ());
+
+      // Regular
+      assertTrue (PDTConfig.setDefaultDateTimeZoneID ("Europe/Berlin").isSuccess ());
+      assertEquals ("Europe/Berlin", PDTConfig.getDefaultDateTimeZone ().getID ());
+
+      // I hope this is not the standard time zone anywhere
+      assertTrue (PDTConfig.setDefaultDateTimeZoneID ("UTC").isSuccess ());
+      assertEquals ("UTC", PDTConfig.getDefaultDateTimeZone ().getID ());
+
+      // The default date time zone was not modified
+      assertEquals (DateTimeZone.getDefault (), PDTConfig.getDefaultChronologyWithoutDateTimeZone ().getZone ());
+
+      if (!DateTimeZone.getDefault ().getID ().equals ("UTC"))
+      {
+        // And therefore this must not be equal
+        assertTrue (!PDTConfig.getDefaultDateTimeZone ().equals (PDTConfig.getDefaultChronologyWithoutDateTimeZone ()
+                                                                          .getZone ()));
+      }
+    }
+    finally
+    {
+      assertTrue (PDTConfig.setDefaultDateTimeZoneID (PDTConfig.DEFAULT_DATETIMEZONEID).isSuccess ());
+    }
+  }
+}
