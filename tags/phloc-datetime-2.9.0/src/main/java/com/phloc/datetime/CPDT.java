f4a058a731e232e3bd4edfbdafedd5365e6f8a8a
ph@phloc.com <ph@phloc.com@47ddc20f-e4ca-fe23-0b93-bd64df41ddb8>
2012-01-30 07:15:40 +0000

    [maven-release-plugin]  copy for tag phloc-datetime-2.9.0

---
+++ tags/phloc-datetime-2.9.0/src/main/java/com/phloc/datetime/CPDT.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.datetime;
+
+import javax.annotation.concurrent.Immutable;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.joda.time.Duration;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.Period;
+
+import com.phloc.commons.annotations.PresentForCodeCoverage;
+
+/**
+ * Some date/time related constants.
+ * 
+ * @author philip
+ */
+@Immutable
+public final class CPDT
+{
+  public static final int START_OF_WEEK_DAY = DateTimeConstants.MONDAY;
+  public static final int END_OF_WEEK_DAY = DateTimeConstants.SUNDAY;
+
+  public static final LocalDate NULL_LOCAL_DATE = PDTFactory.createLocalDate (1970, DateTimeConstants.JANUARY, 1);
+  public static final LocalTime NULL_LOCAL_TIME = PDTFactory.createLocalTime (0, 0, 0);
+  public static final LocalDateTime NULL_LOCAL_DATETIME = NULL_LOCAL_DATE.toLocalDateTime (NULL_LOCAL_TIME);
+  public static final DateTime NULL_DATETIME = PDTFactory.createDateTimeFromMillis (0);
+  public static final Period NULL_PERIOD = new Period ();
+  public static final Duration NULL_DURATION = new Duration (0);
+
+  public static final int LAST_JULIAN_YEAR = 1583;
+
+  @PresentForCodeCoverage
+  @SuppressWarnings ("unused")
+  private static final CPDT s_aInstance = new CPDT ();
+
+  private CPDT ()
+  {}
+}
---
+++ tags/phloc-datetime-2.9.0/src/test/java/com/phloc/datetime/xml/PDTXMLConverterTest.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.datetime.xml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.junit.Test;
+
+import com.phloc.commons.CGlobal;
+import com.phloc.datetime.PDTFactory;
+import com.phloc.datetime.config.PDTConfig;
+
+/**
+ * Test class for class {@link PDTXMLConverter}
+ * 
+ * @author philip
+ */
+public final class PDTXMLConverterTest
+{
+  @Test
+  public void testCreateNewCalendar ()
+  {
+    final XMLGregorianCalendar c1 = PDTXMLConverter.createNewCalendar ();
+    assertNotNull (c1);
+    final XMLGregorianCalendar c2 = PDTXMLConverter.createNewCalendar ();
+    assertNotNull (c2);
+    assertTrue (c1 != c2);
+    assertEquals (c1, c2);
+  }
+
+  @Test
+  public void testLocalDate ()
+  {
+    assertNull (PDTXMLConverter.getXMLCalendarDate (null));
+    final LocalDate aLD = PDTFactory.getCurrentLocalDate ();
+    final XMLGregorianCalendar c1 = PDTXMLConverter.getXMLCalendarDate (aLD);
+    assertNotNull (c1);
+    assertEquals (aLD.getYear (), c1.getYear ());
+    assertEquals (aLD.getMonthOfYear (), c1.getMonth ());
+    assertEquals (aLD.getDayOfMonth (), c1.getDay ());
+    assertEquals (DatatypeConstants.FIELD_UNDEFINED, c1.getHour ());
+    assertEquals (DatatypeConstants.FIELD_UNDEFINED, c1.getMinute ());
+    assertEquals (DatatypeConstants.FIELD_UNDEFINED, c1.getSecond ());
+    assertEquals (DatatypeConstants.FIELD_UNDEFINED, c1.getMillisecond ());
+    assertEquals (0, c1.getTimezone ());
+    final LocalDate aLD2 = PDTXMLConverter.getLocalDate (c1);
+    assertNotNull (aLD2);
+    assertEquals (aLD, aLD2);
+    assertNull (PDTXMLConverter.getLocalDate (null));
+  }
+
+  @Test
+  public void testLocalTime ()
+  {
+    assertNull (PDTXMLConverter.getXMLCalendarTime (null));
+    final LocalTime aLT = PDTFactory.getCurrentLocalTime ();
+    final XMLGregorianCalendar c1 = PDTXMLConverter.getXMLCalendarTime (aLT);
+    assertNotNull (c1);
+    assertEquals (DatatypeConstants.FIELD_UNDEFINED, c1.getYear ());
+    assertEquals (DatatypeConstants.FIELD_UNDEFINED, c1.getMonth ());
+    assertEquals (DatatypeConstants.FIELD_UNDEFINED, c1.getDay ());
+    assertEquals (aLT.getHourOfDay (), c1.getHour ());
+    assertEquals (aLT.getMinuteOfHour (), c1.getMinute ());
+    assertEquals (aLT.getSecondOfMinute (), c1.getSecond ());
+    assertEquals (aLT.getMillisOfSecond (), c1.getMillisecond ());
+    assertEquals (0, c1.getTimezone ());
+    final LocalTime aLT2 = PDTXMLConverter.getLocalTime (c1);
+    assertNotNull (aLT2);
+    assertEquals (aLT, aLT2);
+    assertNull (PDTXMLConverter.getLocalTime (null));
+  }
+
+  @Test
+  public void testLocalDateTime ()
+  {
+    assertNull (PDTXMLConverter.getXMLCalendarDateTime ((LocalDateTime) null));
+    final LocalDateTime aLDT = PDTFactory.getCurrentLocalDateTime ();
+    final XMLGregorianCalendar c1 = PDTXMLConverter.getXMLCalendarDateTime (aLDT);
+    assertNotNull (c1);
+    assertEquals (aLDT.getYear (), c1.getYear ());
+    assertEquals (aLDT.getMonthOfYear (), c1.getMonth ());
+    assertEquals (aLDT.getDayOfMonth (), c1.getDay ());
+    assertEquals (aLDT.getHourOfDay (), c1.getHour ());
+    assertEquals (aLDT.getMinuteOfHour (), c1.getMinute ());
+    assertEquals (aLDT.getSecondOfMinute (), c1.getSecond ());
+    assertEquals (aLDT.getMillisOfSecond (), c1.getMillisecond ());
+    assertEquals (0, c1.getTimezone ());
+    final LocalDateTime aLT2 = PDTXMLConverter.getLocalDateTime (c1);
+    assertNotNull (aLT2);
+    assertEquals (aLDT, aLT2);
+    assertNull (PDTXMLConverter.getLocalDateTime (null));
+  }
+
+  @Test
+  public void testDateTime ()
+  {
+    assertNull (PDTXMLConverter.getXMLCalendarDateTime ((DateTime) null));
+    final DateTime aLDT = PDTFactory.getCurrentDateTime ();
+    final XMLGregorianCalendar c1 = PDTXMLConverter.getXMLCalendarDateTime (aLDT);
+    assertNotNull (c1);
+    assertEquals (aLDT.getYear (), c1.getYear ());
+    assertEquals (aLDT.getMonthOfYear (), c1.getMonth ());
+    assertEquals (aLDT.getDayOfMonth (), c1.getDay ());
+    assertEquals (aLDT.getHourOfDay (), c1.getHour ());
+    assertEquals (aLDT.getMinuteOfHour (), c1.getMinute ());
+    assertEquals (aLDT.getSecondOfMinute (), c1.getSecond ());
+    assertEquals (aLDT.getMillisOfSecond (), c1.getMillisecond ());
+    assertEquals (aLDT.getChronology ().getZone ().getOffset (aLDT) / CGlobal.MILLISECONDS_PER_MINUTE,
+                  c1.getTimezone ());
+    final DateTime aLDT2 = PDTXMLConverter.getDateTime (c1);
+    assertNotNull (aLDT2);
+    assertEquals (aLDT.withChronology (PDTConfig.getDefaultChronology ()),
+                  aLDT2.withChronology (PDTConfig.getDefaultChronology ()));
+    assertNull (PDTXMLConverter.getDateTime (null));
+  }
+}
