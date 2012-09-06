7ae0c63bb82d436e46651fcb4d92daa08470c43a
ph@phloc.com <ph@phloc.com@47ddc20f-e4ca-fe23-0b93-bd64df41ddb8>
2012-09-06 14:18:06 +0000

    [maven-release-plugin]  copy for tag phloc-datetime-3.0.0

---
+++ tags/phloc-datetime-3.0.0/src/main/java/com/phloc/datetime/expiration/IReadonlyExpirableWithReplacement.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.datetime.expiration;
+
+import javax.annotation.Nullable;
+
+/**
+ * Read-only interface for objects that can expire but offer a replacement once
+ * the object expires.
+ * 
+ * @author philip
+ * @param <DATATYPE>
+ *        The type of the object use for defining a replacement.
+ */
+public interface IReadonlyExpirableWithReplacement <DATATYPE> extends IReadonlyExpirable
+{
+  /**
+   * In case the object has an expiration date defined and is expired, the
+   * object returned by this method directs to replacement object to be used
+   * instead.
+   * 
+   * @return <code>null</code> if no replacement is defined, a non-
+   *         <code>null</code> object otherwise.
+   */
+  @Nullable
+  DATATYPE getReplacement ();
+}
---
+++ tags/phloc-datetime-3.0.0/src/test/java/com/phloc/datetime/comparators/ComparatorLocalDateTimeTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.datetime.comparators;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.joda.time.LocalDateTime;
+import org.junit.Test;
+
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.commons.compare.ESortOrder;
+import com.phloc.datetime.PDTFactory;
+
+/**
+ * Test class for class {@link ComparatorLocalDateTime}.
+ * 
+ * @author philip
+ */
+public final class ComparatorLocalDateTimeTest
+{
+  @Test
+  public void testAll ()
+  {
+    final LocalDateTime aObj1 = PDTFactory.createLocalDateTime (2000, 5, 5);
+    final LocalDateTime aObj2 = PDTFactory.createLocalDateTime (1000, 5, 5);
+    final List <LocalDateTime> aList = ContainerHelper.newList (aObj1, aObj2);
+    List <LocalDateTime> aSorted = ContainerHelper.getSorted (aList, new ComparatorLocalDateTime ());
+    assertEquals (aObj2, aSorted.get (0));
+    assertEquals (aObj1, aSorted.get (1));
+    aSorted = ContainerHelper.getSorted (aList, new ComparatorLocalDateTime (ESortOrder.DESCENDING));
+    assertEquals (aObj1, aSorted.get (0));
+    assertEquals (aObj2, aSorted.get (1));
+  }
+}
