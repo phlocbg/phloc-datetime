53b3a6771d1dad26fbfe909c76e7ea9997e605d9
ph@phloc.com <ph@phloc.com@47ddc20f-e4ca-fe23-0b93-bd64df41ddb8>
2014-05-20 17:54:11 +0000

    [maven-release-plugin]  copy for tag phloc-datetime-jdk5-3.1.3

---
+++ tags/phloc-datetime-jdk5-3.1.3/src/main/java/com/phloc/datetime/period/DateTimePeriod.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C) 2006-2014 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.datetime.period;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.Period;
+
+import com.phloc.datetime.PDTFactory;
+
+/**
+ * Default implementation of the {@link IDateTimePeriod} interface.
+ * 
+ * @author Philip Helger
+ */
+@NotThreadSafe
+public class DateTimePeriod extends AbstractFlexiblePeriod <DateTime> implements IDateTimePeriod
+{
+  public DateTimePeriod ()
+  {
+    this (null, null);
+  }
+
+  public DateTimePeriod (@Nullable final DateTime aStart)
+  {
+    this (aStart, null);
+  }
+
+  public DateTimePeriod (@Nullable final DateTime aStart, @Nullable final DateTime aEnd)
+  {
+    super (aStart, aEnd);
+  }
+
+  public final boolean isValidFor (@Nonnull final DateTime aDate)
+  {
+    if (aDate == null)
+      throw new NullPointerException ("date");
+
+    final DateTime aStart = getStart ();
+    if (aStart != null && aStart.isAfter (aDate))
+      return false;
+    final DateTime aEnd = getEnd ();
+    if (aEnd != null && aEnd.isBefore (aDate))
+      return false;
+    return true;
+  }
+
+  public final boolean isValidForNow ()
+  {
+    return isValidFor (PDTFactory.getCurrentDateTime ());
+  }
+
+  public boolean canConvertToPeriod ()
+  {
+    return getStart () != null && getEnd () != null;
+  }
+
+  @Nonnull
+  public final Period getAsPeriod ()
+  {
+    if (!canConvertToPeriod ())
+      throw new IllegalStateException ("Cannot convert to a Period!");
+    return new Period (getStart (), getEnd ());
+  }
+
+  public boolean canConvertToInterval ()
+  {
+    return getStart () != null && getEnd () != null;
+  }
+
+  @Nonnull
+  public Interval getAsInterval ()
+  {
+    if (!canConvertToInterval ())
+      throw new IllegalStateException ("Cannot convert to an Interval!");
+    return new Interval (getStart (), getEnd ());
+  }
+}
